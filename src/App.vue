f3f5632d4913be0a1f0ceab1a033894f493f4424
DimasAditya2 <esadimasadityaa@gmail.com>
2024-12-06 22:59:05 +0700

    feat: add balance feature to display user balance, income, and expense

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-  <div>
-    <HeaderComponent />
+  <HeaderComponent />
+  <div class="container">
+    <Balance :total="+total" />
+    <IncomeExpenses :income="income" :expenses="expenses" />
   </div>
 </template>
 
-<script>
+<script setup>
+// import component - component
 import HeaderComponent from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
 
-export default {
-  components: {
-    HeaderComponent,
-  }
-}
+import { ref, computed } from "vue";
+
+const transactions = ref([
+  { text: "salary", amount: 10000 },
+  { text: "mac mini m4 2024", amount: -599 },
+]);
+
+// Get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+// Get Income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+});
 </script>
